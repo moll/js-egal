30b040a4f7fbdecb3265d8a032121a35bda6a99a
Andri Möll <andri@dot.ee>
2015-05-25 15:52:55 +0300

    Add tests for Symbol.

--- test/index_test.js
+++ test/index_test.js
@@ -151,6 +151,22 @@ describe("egal", function() {
     })
   })
 
+  if (typeof Symbol != "undefined")
+  describe("given Symbol", function() {
+    it("must return true given the same symbol", function() {
+      var symbol = Symbol()
+      egal(symbol, symbol).must.be.true()
+    })
+
+    it("must return false given two anonymous symbols", function() {
+      egal(Symbol(), Symbol()).must.be.false()
+    })
+
+    it("must return false given two named symbols", function() {
+      egal(Symbol("iterator"), Symbol("iterator")).must.be.false()
+    })
+  })
+
   describe("given RegExp", function() {
     it("must return true given equivalent primitives", function() {
       egal(/a/, /a/).must.be.true()
